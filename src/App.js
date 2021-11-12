171631eec315952724e5ce6b6d450534584fcc1d
justin <justinkjoseph17@gmail.com>
2021-11-12 17:40:44 +0530

    discarded lazy loading

--- src/App.js
+++ src/App.js
@@ -5,58 +5,59 @@ import { Spinner } from 'react-bootstrap'
 import { Container } from 'react-bootstrap'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-// import HomeScreen from './Screens/HomeScreen'
-// import ProductScreen from './Screens/ProductScreen'
-// import LoginScreen from './Screens/LoginScreen'
-// import RegisterScreen from './Screens/RegisterScreen'
-// import AdminDashboard from './Screens/AdminDashboard'
-// import ProductManage from './Screens/ProductManage'
-// import UserManage from './Screens/UserManage'
-// import CategoryManage from './Screens/CategoryManage'
-// import OrderManage from './Screens/OrderManage'
-// import ProductEditScreen from './Screens/ProductEditScreen'
-// import CartScreen from './Screens/CartScreen'
-// import ShippingScreen from './Screens/ShippingScreen'
-// import PaymentScreen from './Screens/PaymentScreen'
-// import PlaceOrderScreen from './Screens/PlaceOrderScreen'
-// import OrderScreen from './Screens/OrderScreen'
-// import ProfileScreen from './Screens/ProfileScreen'
-// import CouponManage from './Screens/CouponManage'
+import HomeScreen from './Screens/HomeScreen'
+import ProductScreen from './Screens/ProductScreen'
+import LoginScreen from './Screens/LoginScreen'
+import RegisterScreen from './Screens/RegisterScreen'
+import AdminDashboard from './Screens/AdminDashboard'
+import ProductManage from './Screens/ProductManage'
+import UserManage from './Screens/UserManage'
+import CategoryManage from './Screens/CategoryManage'
+import OrderManage from './Screens/OrderManage'
+import ProductEditScreen from './Screens/ProductEditScreen'
+import CartScreen from './Screens/CartScreen'
+import ShippingScreen from './Screens/ShippingScreen'
+import PaymentScreen from './Screens/PaymentScreen'
+import PlaceOrderScreen from './Screens/PlaceOrderScreen'
+import OrderScreen from './Screens/OrderScreen'
+import ProfileScreen from './Screens/ProfileScreen'
+import CouponManage from './Screens/CouponManage'
 
-//lazy loading
-const HomeScreen = lazy(() => import('./Screens/HomeScreen'))
-const ProductScreen = lazy(() => import('./Screens/ProductScreen'))
-const LoginScreen = lazy(() => import('./Screens/LoginScreen'))
-const RegisterScreen = lazy(() => import('./Screens/RegisterScreen'))
-const AdminDashboard = lazy(() => import('./Screens/AdminDashboard'))
-const ProductManage = lazy(() => import('./Screens/ProductManage'))
-const UserManage = lazy(() => import('./Screens/UserManage'))
-const CategoryManage = lazy(() => import('./Screens/CategoryManage'))
-const OrderManage = lazy(() => import('./Screens/OrderManage'))
-const ProductEditScreen = lazy(() => import('./Screens/ProductEditScreen'))
-const CartScreen = lazy(() => import('./Screens/CartScreen'))
-const ShippingScreen = lazy(() => import('./Screens/ShippingScreen'))
-const PaymentScreen = lazy(() => import('./Screens/PaymentScreen'))
-const PlaceOrderScreen = lazy(() => import('./Screens/PlaceOrderScreen'))
-const OrderScreen = lazy(() => import('./Screens/OrderScreen'))
-const ProfileScreen = lazy(() => import('./Screens/ProfileScreen'))
-const CouponManage = lazy(() => import('./Screens/CouponManage'))
+// //lazy loading
+// const HomeScreen = lazy(() => import('./Screens/HomeScreen'))
+// const ProductScreen = lazy(() => import('./Screens/ProductScreen'))
+// const LoginScreen = lazy(() => import('./Screens/LoginScreen'))
+// const RegisterScreen = lazy(() => import('./Screens/RegisterScreen'))
+// const AdminDashboard = lazy(() => import('./Screens/AdminDashboard'))
+// const ProductManage = lazy(() => import('./Screens/ProductManage'))
+// const UserManage = lazy(() => import('./Screens/UserManage'))
+// const CategoryManage = lazy(() => import('./Screens/CategoryManage'))
+// const OrderManage = lazy(() => import('./Screens/OrderManage'))
+// const ProductEditScreen = lazy(() => import('./Screens/ProductEditScreen'))
+// const CartScreen = lazy(() => import('./Screens/CartScreen'))
+// const ShippingScreen = lazy(() => import('./Screens/ShippingScreen'))
+// const PaymentScreen = lazy(() => import('./Screens/PaymentScreen'))
+// const PlaceOrderScreen = lazy(() => import('./Screens/PlaceOrderScreen'))
+// const OrderScreen = lazy(() => import('./Screens/OrderScreen'))
+// const ProfileScreen = lazy(() => import('./Screens/ProfileScreen'))
+// const CouponManage = lazy(() => import('./Screens/CouponManage'))
 
 const App = () => {
   return (
-    <Suspense
-      fallback={
-        <Spinner
-          animation='border'
-          role='status'
-          style={{
-            width: '100px',
-            height: '100px',
-            margin: 'auto',
-            display: 'block',
-          }}
-        ></Spinner>
-      }
+    <
+      // Suspense
+      // fallback={
+      //   <Spinner
+      //     animation='border'
+      //     role='status'
+      //     style={{
+      //       width: '100px',
+      //       height: '100px',
+      //       margin: 'auto',
+      //       display: 'block',
+      //     }}
+      //   ></Spinner>
+      // }
     >
       <Router>
         <Header />
@@ -87,7 +88,7 @@ const App = () => {
         </main>
         <Footer />
       </Router>
-    </Suspense>
+    </>
   )
 }
 
